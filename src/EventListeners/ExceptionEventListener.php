afac53cf7eb4a03abe9cdefdcd362761ab61ee0c
panteleyR <panteley98@bk.ru>
2022-06-19 01:01:50 +0300

    test

--- src/EventListeners/ExceptionEventListener.php
+++ src/EventListeners/ExceptionEventListener.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace Lilith\HttpKernel\EventListeners;
 
+use Lilith\Http\Message\Response;
 use Lilith\HttpKernel\Events\ExceptionEvent;
 
 class ExceptionEventListener
 {
-    public function __invoke(ExceptionEvent $exceptionEvent): void
+    public function __invoke(ExceptionEvent $exceptionEvent): ExceptionEvent
     {
-        if ($exceptionEvent->getResponse() === null) {
-//            $exceptionEvent->setResponse();
-        }
+        $message =  'error: ' . $exceptionEvent->getThrowable()->getTraceAsString() . $exceptionEvent->getThrowable()->getMessage();
+        $response = new Response(500, [], $message);
+        $exceptionEvent->setResponse($response);
+
+        return $exceptionEvent;
     }
 }
--- src/Kernel.php
+++ src/Kernel.php
@@ -12,17 +12,21 @@ use Lilith\Env\Env;
 use Lilith\Env\EnvInterface;
 use Lilith\EventDispatcher\EventDispatcher;
 use Lilith\EventDispatcher\EventDispatcherInterface;
+use Lilith\EventDispatcher\EventDispatcherProvider;
 use Lilith\Http\Message\RequestInterface;
 use Lilith\Http\Message\ResponseInterface;
 use Lilith\HttpKernel\Events\ExceptionEvent;
+use Lilith\Router\Router;
 use Lilith\Router\RouterInterface;
 
 class Kernel
 {
     protected ContainerInterface $container;
-    protected string $environment;
-
-    public function __construct(protected bool $debug) {}
+    protected Env $env;
+//
+//    public function __construct(protected bool $debug) {
+//
+//    }
 
     public function handle(RequestInterface $request): ResponseInterface
     {
@@ -48,14 +52,14 @@ class Kernel
         return $controller->{$method}($request);
     }
 
-    protected function handleThroughEventBus(RequestInterface $request): ResponseInterface
-    {
-        $router = $this->container->get('router');
-        $event = $router->findRoute($request);
-        $event = $this->container->get('eventDispatcher')->dispatch($event);
-
-        return $event->getResponse();
-    }
+//    protected function handleThroughEventBus(RequestInterface $request): ResponseInterface
+//    {
+//        $router = $this->container->get('router');
+//        $event = $router->findRoute($request);
+//        $event = $this->container->get('eventDispatcher')->dispatch($event);
+//
+//        return $event->getResponse();
+//    }
 
     protected function handleThrowable(\Throwable $e, RequestInterface $request): ResponseInterface
     {
@@ -65,40 +69,30 @@ class Kernel
         return $event->getResponse();
     }
 
-    protected function getKernelParameters(): array
-    {
-        return [
-            'kernel.project_dir' => realpath($this->getProjectDir()) ?: $this->getProjectDir(),
-//            'kernel.environment' => $this->environment,
-//            'kernel.runtime_environment' => '%env(default:kernel.environment:APP_RUNTIME_ENV)%',
-//            'kernel.debug' => $this->debug,
-//            'kernel.build_dir' => realpath($buildDir = $this->warmupDir ?: $this->getBuildDir()) ?: $buildDir,
-//            'kernel.cache_dir' => realpath($cacheDir = ($this->getCacheDir() === $this->getBuildDir() ? ($this->warmupDir ?: $this->getCacheDir()) : $this->getCacheDir())) ?: $cacheDir,
-//            'kernel.logs_dir' => realpath($this->getLogDir()) ?: $this->getLogDir(),
-//            'kernel.charset' => $this->getCharset(),
-//            'kernel.container_class' => $this->getContainerClass(),
-        ];
-    }
-
     protected function boot(): void
     {
-        chdir(__DIR__ . '/..');
+        chdir(dirname($_SERVER["SCRIPT_FILENAME"]) . '/..');
+        $this->initilizeEnv();
         $this->initilizeContainer();
-        $this->container->get('env')->load($this->getProjectDir() . '/.env');
-        $this->container->get('router')->setRoutes($this->container->getParameter('package.routes'));
-        $eventDispatcherConfig = $this->container->getParameter('package.eventListeners') ?? [];
-        foreach ($eventDispatcherConfig as $event => $listenerList) {
-            foreach ($listenerList as $listenerClass => $listenerConfig) {
-                $object = $this->container->get($listenerClass);
-                $this->container->get('eventDispatcher')->addListener(
-                    $object,
-                    $event,
-                    $listenerConfig['method'] ?? null,
-                    $listenerConfig['priority'] ?? 1,
-                );
-            }
+//        $this->container->get('router')->setRoutes($this->container->getParameter('package.routes'));
+//        $eventDispatcherConfig = $this->container->getParameter('package.event_listeners') ?? [];
+//        foreach ($eventDispatcherConfig as $event => $listenerList) {
+//            foreach ($listenerList as $listenerClass => $listenerConfig) {
+//                $object = $this->container->get($listenerClass);
+//                $this->container->get('eventDispatcher')->addListener(
+//                    $object,
+//                    $event,
+//                    $listenerConfig['method'] ?? null,
+//                    $listenerConfig['priority'] ?? 1,
+//                );
+//            }
+//        }
+    }
 
-        }
+    protected function initilizeEnv(): void
+    {
+        $this->env = new Env();
+        $this->env->load($this->getProjectDir() . '/.env');
     }
 
     protected function getProjectDir(): string
@@ -119,16 +113,28 @@ class Kernel
         $containerBuilder->addParameters($this->getKernelParameters());
         $containerBuilder->addServices($this->getKernelServices());
         $this->container = $containerBuilder->build($containerConfigurator);
-        $this->container->set('container', $this->container);
     }
 
     protected function getKernelServices(): array
     {
         return [
             [['eventDispatcher', EventDispatcherInterface::class], EventDispatcher::class],
-            [['router', RouterInterface::class], RouterInterface::class],
-            [['env', EnvInterface::class], Env::class],
+            [['router', RouterInterface::class], Router::class],
 //            [['cache', CacheInterface::class], Cache::class],
+        ];
+    }
+
+    protected function getKernelParameters(): array
+    {
+        return [
+            'env' => $this->env->getAll(),
+            'kernel.project_dir' => realpath($this->getProjectDir()) ?: $this->getProjectDir(),
+            'kernel.environment' => $this->env->get('APP_DEV'),
+//            'kernel.debug' => $this->debug,
+//            'kernel.build_dir' => realpath($buildDir = $this->warmupDir ?: $this->getBuildDir()) ?: $buildDir,
+//            'kernel.cache_dir' => realpath($cacheDir = ($this->getCacheDir() === $this->getBuildDir() ? ($this->warmupDir ?: $this->getCacheDir()) : $this->getCacheDir())) ?: $cacheDir,
+//            'kernel.logs_dir' => realpath($this->getLogDir()) ?: $this->getLogDir(),
+//            'kernel.charset' => $this->getCharset(),
         ];
     }
 
@@ -136,14 +142,15 @@ class Kernel
     {
         $configDir = $this->getConfigDir();
 
-        $containerConfigurator->importPackage($configDir . '/{packages}/*.yaml');
-        $containerConfigurator->importPackage($configDir . '/{packages}/' . $this->environment.'/*.yaml');
+        $containerConfigurator->importPackage($configDir . '/packages/*.yaml');
+//        $containerConfigurator->importPackage($configDir . '/packages/' . $this->env->get('APP_DEV') . '/*.yaml');
 
         $containerConfigurator->import($configDir . '/services.yaml');
-        $containerConfigurator->import($configDir . '/services_' . $this->environment.'.yaml');
+        $containerConfigurator->import($configDir . '/services_' . $this->env->get('APP_DEV') .'.yaml');
 
-        $containerConfigurator->importPackage($configDir . '/routes/' . $this->environment.'/*.yaml');
-        $containerConfigurator->importPackage($configDir . '/routes/*.yaml');
         $containerConfigurator->importPackage($configDir . '/routes.yaml');
+//        $containerConfigurator->importPackage($configDir . '/routes/' . $this->env->get('APP_DEV') . '/*.yaml');
+//        $containerConfigurator->importPackage($configDir . '/routes/*.yaml');
+        $containerConfigurator->importPackage($configDir . '/event_listeners.yaml');
     }
 }
